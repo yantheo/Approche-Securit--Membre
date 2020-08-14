7ea30016efeaa735ac100067957829ea0509c244
Yanick Theodose <yanick.theodose@outlook.com>
2020-08-14 16:40:39 +0200

    formulaire connection

--- connexion.php
+++ connexion.php
@@ -1,3 +1,12 @@
 <?php 
 $mysqli = new Mysqli("localhost", "root", "", "securite");
 ?>
+<hr>
+
+<form method="post" action="">
+	<label for="pseudo">Pseudo</label><br>
+	<input type="text" id="pseudo" name="pseudo">
+	<label for="mdp">Mot de passe</label>
+	<input type="text" id="mdp" name="mdp">
+	<input type="submit" value="Se connecter">
+</form>
